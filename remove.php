e11bf28fe61b8589cb63da24ea630be3618c76b0
Denis Chernovs <chernoff.cs@gmail.com>
2023-06-02 01:54:01 +0300

    update

--- remove.php
+++ remove.php
@@ -11,7 +11,7 @@ $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// Remove by id
+// Remove by id 
 $sql = "DELETE FROM question WHERE id = $id";
 
 if ($conn->query($sql) === TRUE) {
